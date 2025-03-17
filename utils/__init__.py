658f12e706d8ffd2b34a150fa8dc1e7ca2d0ea27
Shimesaba0704 <shuheikono03@gmail.com>
2025-03-17 18:46:42 +0900

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,10 @@
 from utils.state import create_initial_state
-from utils.theme import setup_langgraph_theme
+from utils.theme import apply_theme_styles
 from utils.api_client import get_client, initialize_client, update_model, get_available_models
 
 __all__ = [
     'create_initial_state', 
-    'setup_langgraph_theme', 
+    'apply_theme_styles', 
     'get_client', 
     'initialize_client', 
     'update_model',
