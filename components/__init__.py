398893f76d58bbc0989c2dce1d8f249c587f924e
Shimesaba0704 <shuheikono03@gmail.com>
2025-03-17 17:03:12 +0900

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,5 +1,5 @@
 from components.sidebar import render_sidebar
 from components.workflow_viz import render_workflow_visualization
-from components.dialog_history import display_dialog_history, add_to_dialog_history
+from components.dialog_history import display_dialog_history, add_to_dialog_history, update_dialog_display
 
-__all__ = ['render_sidebar', 'render_workflow_visualization', 'display_dialog_history', 'add_to_dialog_history']
+__all__ = ['render_sidebar', 'render_workflow_visualization', 'display_dialog_history', 'add_to_dialog_history', 'update_dialog_display']
